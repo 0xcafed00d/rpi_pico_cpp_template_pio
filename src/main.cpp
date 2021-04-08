a7d8e8183bc4cef624882dd393b619928377069f
Lee Witek <5556121+0xcafed00d@users.noreply.github.com>
2021-04-08 12:04:19 +0100

    added pio program

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 
 #include <stdio.h>
+#include "hardware/pio.h"
 #include "pico/binary_info.h"
 #include "pico/stdlib.h"
 
+#include "template.pio.h"
+
 int main() {
-	bi_decl(bi_program_description("template raspberry pi pico c++ project"));
+	bi_decl(bi_program_description("template raspberry pi pico c++ project with pio"));
 
 	stdio_init_all();
 
@@ -13,12 +16,24 @@ int main() {
 	gpio_init(LED_PIN);
 	gpio_set_dir(LED_PIN, GPIO_OUT);
 
-	uint32_t count = 1;
+	// use pio0
+	PIO pio = pio0;
+
+	// load program
+	uint offset = pio_add_program(pio, &template_program);
+
+	// get a state machine
+	uint sm = pio_claim_unused_sm(pio, true);
+
+	// init program
+	template_program_init(pio, sm, offset, PICO_DEFAULT_LED_PIN);
 
 	while (true) {
-		gpio_put(LED_PIN, count & 1);
-		printf("%d\n", count);
-		sleep_ms(100);
-		count++;
+		// put 1 onto input fifo
+		pio_sm_put_blocking(pio, sm, 1);
+		sleep_ms(500);
+		// put 0 onto input fifo
+		pio_sm_put_blocking(pio, sm, 0);
+		sleep_ms(500);
 	}
 }
